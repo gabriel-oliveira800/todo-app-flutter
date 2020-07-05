19609654f41d31211e409c8e0e0ebb042e8487f2
gabrielscorehero <gabrielscorehero55@gmail.com>
2020-07-05 10:34:02 -0300

    init project todoApp and add http puspec.yaml

---
+++ android/app/src/main/kotlin/br/com/gabrielscorehero/todoApp/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.gabrielscorehero.todoApp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(TodoApp());
+
+class TodoApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+    );
+  }
+}
